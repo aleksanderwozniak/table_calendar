77b7bdf23d9804d275fd5d49cab83d3ff6aa243a
divlov <63954908+divlov@users.noreply.github.com>
2023-11-08 18:52:16 +0530

    make chevrons render just beside the header title

--- lib/src/widgets/calendar_header.dart
+++ lib/src/widgets/calendar_header.dart
@@ -56,20 +56,18 @@ class CalendarHeader extends StatelessWidget {
               margin: headerStyle.leftChevronMargin,
               padding: headerStyle.leftChevronPadding,
             ),
-          Expanded(
-            child: headerTitleBuilder?.call(context, focusedMonth) ??
-                GestureDetector(
-                  onTap: onHeaderTap,
-                  onLongPress: onHeaderLongPress,
-                  child: Text(
-                    text,
-                    style: headerStyle.titleTextStyle,
-                    textAlign: headerStyle.titleCentered
-                        ? TextAlign.center
-                        : TextAlign.start,
-                  ),
+          headerTitleBuilder?.call(context, focusedMonth) ??
+              GestureDetector(
+                onTap: onHeaderTap,
+                onLongPress: onHeaderLongPress,
+                child: Text(
+                  text,
+                  style: headerStyle.titleTextStyle,
+                  textAlign: headerStyle.titleCentered
+                      ? TextAlign.center
+                      : TextAlign.start,
                 ),
-          ),
+              ),
           if (headerStyle.formatButtonVisible &&
               availableCalendarFormats.length > 1)
             Padding(
