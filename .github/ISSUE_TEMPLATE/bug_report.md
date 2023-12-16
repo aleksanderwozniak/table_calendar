34cd596aea97a02fca79b3ed97cdca28d8fdbbf1
Treamz <chrnknzhnkv@gmail.com>
2023-12-16 22:29:43 +0100

    Added CalendarFormat.day

---
+++ .github/ISSUE_TEMPLATE/bug_report.md
@@ -0,0 +1,38 @@
+---
+name: Bug report
+about: Report unexpected behavior
+title: ''
+labels: bug
+assignees: ''
+
+---
+
+**Describe the bug**
+A clear and concise description of what the bug is.
+
+**To reproduce**
+Please include a short code sample that can be used to reproduce the problem.
+<details>
+<summary>Code sample</summary>
+</details>
+
+Steps to reproduce the behavior:
+1. Go to '...'
+2. Click on '....'
+3. Scroll down to '....'
+4. See error
+
+**Expected behavior**
+A clear and concise description of what you expected to happen.
+
+**Screenshots**
+If applicable, add screenshots to help explain your problem.
+
+**Output of `flutter doctor`**
+Paste the result of this command here.
+<details>
+<summary>Output</summary>
+</details>
+
+**Additional context**
+Add any other context about the problem here.
---
+++ lib/src/widgets/calendar_page.dart
@@ -0,0 +1,111 @@
+// Copyright 2019 Aleksander Woźniak
+// SPDX-License-Identifier: Apache-2.0
+
+import 'package:flutter/widgets.dart';
+
+class CalendarPage extends StatelessWidget {
+  final Widget Function(BuildContext context, DateTime day)? dowBuilder;
+  final Widget Function(BuildContext context, DateTime day) dayBuilder;
+  final Widget Function(BuildContext context, DateTime day)? weekNumberBuilder;
+  final List<DateTime> visibleDays;
+  final Decoration? dowDecoration;
+  final Decoration? rowDecoration;
+  final TableBorder? tableBorder;
+  final EdgeInsets? tablePadding;
+  final bool dowVisible;
+  final bool weekNumberVisible;
+  final double? dowHeight;
+
+  const CalendarPage({
+    Key? key,
+    required this.visibleDays,
+    this.dowBuilder,
+    required this.dayBuilder,
+    this.weekNumberBuilder,
+    this.dowDecoration,
+    this.rowDecoration,
+    this.tableBorder,
+    this.tablePadding,
+    this.dowVisible = true,
+    this.weekNumberVisible = false,
+    this.dowHeight,
+  })  : assert(!dowVisible || (dowHeight != null && dowBuilder != null)),
+        assert(!weekNumberVisible || weekNumberBuilder != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: tablePadding ?? EdgeInsets.zero,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          if (weekNumberVisible) _buildWeekNumbers(context),
+          if (visibleDays.length == 1) Expanded(child: _buildCalendarDay(context)),
+          if (visibleDays.length > 1) Expanded(
+            child: Table(
+              border: tableBorder,
+              children: [
+                if (dowVisible) _buildDaysOfWeek(context),
+                ..._buildCalendarDays(context),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildWeekNumbers(BuildContext context) {
+    final rowAmount = visibleDays.length ~/ 7;
+    return Column(
+      children: [
+        if (dowVisible) SizedBox(height: dowHeight ?? 0),
+        ...List.generate(rowAmount, (index) => index * 7)
+            .map((index) => Expanded(
+                  child: weekNumberBuilder!(context, visibleDays[index]),
+                ))
+            .toList()
+      ],
+    );
+  }
+
+  TableRow _buildDaysOfWeek(BuildContext context) {
+    if (visibleDays.length == 1) {
+      return TableRow(
+        decoration: dowDecoration,
+        children: List.generate(
+          1,
+              (index) => dowBuilder!(context, visibleDays[index]),
+        ).toList(),
+      );
+    }
+    return TableRow(
+      decoration: dowDecoration,
+      children: List.generate(
+        7,
+        (index) => dowBuilder!(context, visibleDays[index]),
+      ).toList(),
+    );
+  }
+
+  List<TableRow> _buildCalendarDays(BuildContext context) {
+    final rowAmount = visibleDays.length ~/ 7;
+
+    return List.generate(rowAmount, (index) => index * 7)
+        .map((index) => TableRow(
+              decoration: rowDecoration,
+              children: List.generate(
+                7,
+                (id) => dayBuilder(context, visibleDays[index + id]),
+              ),
+            ))
+        .toList();
+  }
+
+  Widget _buildCalendarDay(BuildContext context) {
+    return Container(
+      decoration: rowDecoration,
+      child: Center(child: dayBuilder(context, visibleDays[0])),);
+  }
+}
